3b9586e7f48e5f780272fbad2f2c5c64eb570422
hsok0070 <rlaalsdn777@hanmir.com>
2020-06-05 16:19:13 +0900

    [ADD] (라우터 공부) views폴더에 test.vue 생성후 router 등록후 App.vue router-link 추가!

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/', // URL 뒤에 붙는 경로를 말한다.
     name: 'Home',
     component: Home
   },
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: () => import('../views/test.vue')
   }
 ]
 
