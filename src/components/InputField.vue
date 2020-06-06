54d16c1a071c89a75ff320a45486154c16c4f785
hsok0070 <rlaalsdn777@hanmir.com>
2020-06-06 17:40:18 +0900

    [ADD] (부모 컴포넌트로 데이터 보내기 공부) InputField.vue(자식컴포넌트)에서 Home.vue(부모컴포넌트)로 emit,event를 사용하여 데이터 전달하기!

---
+++ src/components/InputField.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <label for="">Name : </label>
+        <input
+        type="text"
+        :value="name"
+        style="padding: 5px; border: 2px solid green;"
+        @input="$emit('update-name', $event)"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: false
+    }
+  }
+  /* methods: {
+    updateName (e) {
+      console.log(e.target.value)
+      this.$emit('update-name', e.target.value)
+    }
+  } */
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,35 @@
 <template>
   <div id='app'>
     <h1>This is Home page</h1>
-    <MinWoo title='home title' name="MinWoo"></MinWoo>
+    <!-- <MinWoo title='home title' name="MinWoo"></MinWoo> --> <!-- 부모 컴포넌트로 데이터 보내기 공부 하기 위해 주석 -->
+    <form action="">
+      <InputField @update-name='name = $event.target.value'></InputField>
+      <!-- <InputField v-model="name"></InputField --> <!-- 위 $event를 간단히 사용하는방법 -->
+      <br><br><button>Submit</button>
+    </form>
+    {{ name }}
   </div>
 </template>
 
 <script>
-import MinWoo from '@/components/MinWoo.vue' /* @는 src위치를 가르킨다. */
+/* import MinWoo from '@/components/MinWoo.vue' */
+import InputField from '@/components/InputField.vue'
 
 export default {
   components: {
-    MinWoo
+    InputField
   },
   data () {
     return {
-      name: 'Min Woo'
+      name: ''
 
     }
   }
+  /* methods: {
+    updateName (name) {
+      this.name = name
+    }
+  } */
 }
 
 </script>
